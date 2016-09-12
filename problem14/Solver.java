6d0f9c7475d4256606912de95e586e2cdcd9b6f1
bgibbons0531 <b180g073@ku.edu>
2016-09-12 12:46:40 -0500

    Update Solver.java

--- problem14/Solver.java
+++ problem14/Solver.java
@@ -1,3 +1,9 @@
+/**
+*	@file: Solver.java
+* 	@author: Brad Gibbons
+* 	@date: 2016.09.12
+* 	Description: Calculate the starting number under 1 million that produces the longets Collatz sequence.
+*/
 
 public class Solver {
 	public static void main(String[] args) {
