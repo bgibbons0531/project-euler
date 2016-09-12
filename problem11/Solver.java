2cba7ad1a707a87fe6877e15ac4beabcab7ad13f
bgibbons0531 <b180g073@ku.edu>
2016-09-12 12:40:14 -0500

    Added file heading and problem description

--- problem11/Solver.java
+++ problem11/Solver.java
@@ -1,3 +1,10 @@
+/**
+*	@file: Solver.java
+* 	@author: Brad Gibbons
+* 	@date: 2016.09.12
+* 	Description: Calculate the largest product of 4 adjacent numbers in a 20x20 gird.
+*/
+
 import java.util.*;
 public class Solver {
 	public static void main(String[] args) {
