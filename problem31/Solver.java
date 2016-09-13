6b53dc18f587da2a0eb663f80d62b697b5d1af10
bgibbons0531 <b180g073@ku.edu>
2016-09-12 23:17:30 -0500

    Update Solver.java

--- problem31/Solver.java
+++ problem31/Solver.java
@@ -1,3 +1,9 @@
+/**
+*	@file: Solver.java
+* 	@author: Brad Gibbons
+* 	@date: 2016.09.12
+* 	Description: Calculate the number of ways 2pounds can be made using any number of coins.
+*/
 
 public class Solver {
 	public static void main(String[] args) {
