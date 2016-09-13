937a2ae2ff44707a6b953d650713d55817decd93
bgibbons0531 <b180g073@ku.edu>
2016-09-12 23:30:13 -0500

    Update Solver.java

--- problem41/Solver.java
+++ problem41/Solver.java
@@ -1,3 +1,10 @@
+/**
+*	@file: Solver.java
+* 	@author: Brad Gibbons
+* 	@date: 2016.09.12
+* 	Description: Calculate the largest n-digit pandigital prime number.
+*/
+
 import java.util.*;
 public class Solver {	
 	public static void main(String[] args) {
