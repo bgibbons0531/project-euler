6556dedb989c03c160e684b796bc5b8c55539259
Brad Gibbons <b180g073@ku.edu>
2016-09-13 12:01:10 -0500

    Convert the list of words into a list of their scores.

--- problem42/Solver.java
+++ problem42/Solver.java
@@ -11,9 +11,26 @@ public class Solver {
 		//Declaration
 		Importer myImporter = new Importer();
 		List<String> wordList = new ArrayList<String>();
+		List<Integer> wordScoreList = new ArrayList<Integer>();
 
 		//Get list of words
 		wordList = myImporter.getWords();
-		
+
+		//Create a list of the score of each word
+		String currentWord = "";
+		int currentSum = 0;
+		for (int i=0; i<wordList.size(); i++) {
+			currentWord = wordList.get(i); //Set currentWord
+			currentSum = 0;
+			for (int j=0; j<currentWord.length(); j++) {
+				currentSum += getLetterScore(currentWord.charAt(j));
+			}
+			wordScoreList.add(currentSum);
+		}
 	}
+
+	//Private methods
+	private static int getLetterScore(char c) {
+		return((int)c - 64);
+	} 
 }
