20ad6b6e3cbc90828d0554c24050da5ba1218bc7
Brad Gibbons <b180g073@ku.edu>
2016-09-13 12:12:11 -0500

    Finished Solver class to calculate the solution

--- problem42/Solver.java
+++ problem42/Solver.java
@@ -9,14 +9,16 @@ import java.util.*;
 public class Solver {
 	public static void main(String[] args) {
 		//Declaration
+		int count = 0;
 		Importer myImporter = new Importer();
 		List<String> wordList = new ArrayList<String>();
 		List<Integer> wordScoreList = new ArrayList<Integer>();
+		List<Integer> triangleNumberList = new ArrayList<Integer>();
 
 		//Get list of words
 		wordList = myImporter.getWords();
 
-		//Create a list of the score of each word
+		//Create a list of the score of each word and sort
 		String currentWord = "";
 		int currentSum = 0;
 		for (int i=0; i<wordList.size(); i++) {
@@ -27,6 +29,27 @@ public class Solver {
 			}
 			wordScoreList.add(currentSum);
 		}
+		Collections.sort(wordScoreList);
+
+		//Create a list of triangle numbers up to the max of wordScoreList
+		int limit = wordScoreList.get(wordScoreList.size()-1); //Highest word score
+		int x = 1;
+		int currentTriangle = 1;
+		while (currentTriangle <= limit) {
+			triangleNumberList.add(currentTriangle); //Add the current triangle number to the list
+			x++; //Increment x
+			currentTriangle = x*(x+1)/2; //Get the next triangle number
+		}
+
+		//Count how many words are triangle numbers
+		for (int i=0; i<wordScoreList.size(); i++) {
+			if (triangleNumberList.contains(wordScoreList.get(i))) {
+				count++;
+			}
+		}
+
+		//Print the solution
+		System.out.println(count);
 	}
 
 	//Private methods
