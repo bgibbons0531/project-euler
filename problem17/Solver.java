9459f44b6aa6c76e0b67594d6f7ca890382458cb
bgibbons0531 <b180g073@ku.edu>
2016-09-12 22:59:42 -0500

    Update Solver.java

--- problem17/Solver.java
+++ problem17/Solver.java
@@ -1,3 +1,9 @@
+/**
+*	@file: Solver.java
+* 	@author: Brad Gibbons
+* 	@date: 2016.09.12
+* 	Description: Calculate the number of letters used to write out the numbers 1 to 1000 inclusive using British usage.
+*/
 
 public class Solver {
 	public static void main(String[] args) {
