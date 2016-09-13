70868f178d6d09a10678f2541047ca2edc9d5d54
Brad Gibbons <b180g073@ku.edu>
2016-09-13 11:32:20 -0500

    Created Solver.java and declared the class and main method.

---
+++ problem42/Solver.java
@@ -0,0 +1,12 @@
+/**
+*	@file: Solver.java
+*	@author: Brad Gibbons
+*	@date: 2016.09.13
+*	Purpose: Calculate the number of triangle words in words.txt.
+*/
+
+import java.util.*;
+public class Solver {
+	public static void main(String[] args) {
+	}
+}
